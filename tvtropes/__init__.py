d3620fa705874b8f546df20733017908b5ee229f
Roman Rader <roman.rader@gmail.com>
2014-06-20 19:14:39 +0300

    film list

--- tvtropes/__init__.py
+++ tvtropes/__init__.py
@@ -1,21 +1,44 @@
 from grab import Grab
 import csv
 
-csvfile = open('data.csv', 'w', newline='')
-writer = csv.writer(csvfile, delimiter=',')
 
+def generate_trope_list(ns, name):
+    csvfile = open('data.csv', 'w', newline='')
+    writer = csv.writer(csvfile, delimiter=',')
 
-def register_trope(name, trope):
-    writer.writerow([name, trope['name'], trope['href']])
+    def register_trope(name, trope):
+        writer.writerow([name, trope['name'], trope['href']])
 
-g = Grab()
-ns = "Film"
-name = "ReturnOfTheJedi"
-g.go("http://tvtropes.org/pmwiki/pmwiki.php/{ns}/{name}".format(ns=ns, name=name))
-tropes = g.doc.select('//div[@id="wikitext"]//ul[1]/li/a[1]')
-for trope in tropes:
-    trope_d = {"name": trope.text(),
-               "href": trope.attr('href')}
-    register_trope(name, trope_d)
+    g = Grab()
+    g.go("http://tvtropes.org/pmwiki/pmwiki.php/{ns}/{name}".format(ns=ns, name=name))
+    tropes = g.doc.select('//div[@id="wikitext"]//ul[1]/li/a[1]')
+    for trope in tropes:
+        trope_d = {"name": trope.text(),
+                   "href": trope.attr('href')}
+        register_trope(name, trope_d)
+    csvfile.close()
 
-csvfile.close()
+def generate_film_list(ns):
+    # csvfile = open('data.csv', 'w', newline='')
+    # writer = csv.writer(csvfile, delimiter=',')
+
+    def register_trope(name, trope):
+        print(name, trope)
+        # writer.writerow([name, trope['name'], trope['href']])
+
+    g = Grab()
+    g.go("http://tvtropes.org/pmwiki/pmwiki.php/{ns}".format(ns=ns))
+    tropes = g.doc.select('//div[@id="wikitext"]/ul[1]/li/a[1]')
+    for trope in tropes:
+        trope_d = {"name": trope.text(),
+                   "href": trope.attr('href')}
+        register_trope(ns, trope_d)
+        # TODO: process every page
+    # csvfile.close()
+
+if __name__ == "__main__":
+    ns = "Film"
+    # name = "ReturnOfTheJedi"
+    # generate_trope_list(ns, name)
+
+    generate_film_list(ns)
